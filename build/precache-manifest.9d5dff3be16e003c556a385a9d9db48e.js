fd9fd0b85d5ae5c9c49f0acab59ded8a9f3d9a8f
victechie <leofizzy@gmail.com>
2020-09-10 02:52:53 +0100

    Added Build Folder

---
+++ build/precache-manifest.9d5dff3be16e003c556a385a9d9db48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac444a93ed23ee6c8a0114f593e2216d",
+    "url": "/pint/index.html"
+  },
+  {
+    "revision": "74245c428dbe6b9fea6a",
+    "url": "/pint/static/css/main.6a1d9214.chunk.css"
+  },
+  {
+    "revision": "af62e1f30fb51dac6f90",
+    "url": "/pint/static/js/2.e336f3c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pint/static/js/2.e336f3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74245c428dbe6b9fea6a",
+    "url": "/pint/static/js/main.9bec99b1.chunk.js"
+  },
+  {
+    "revision": "f0730f59d52831b449c9",
+    "url": "/pint/static/js/runtime-main.b5b50da8.js"
+  }
+]);
